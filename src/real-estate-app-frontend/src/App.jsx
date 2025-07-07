2b18786b2a2b66313672665a1e77f203bf7006bc
Nub-Raghav <raghavlathi0003@gmail.com>
2025-07-07 21:05:36 +0530

    frontend initialized

--- src/real-estate-app-frontend/src/App.jsx
+++ src/real-estate-app-frontend/src/App.jsx
@@ -1,31 +1,42 @@
-import { useState } from 'react';
-import { real_estate_app_backend } from 'declarations/real-estate-app-backend';
+// import { useState } from 'react';
+// import { real_estate_app_backend } from 'declarations/real-estate-app-backend';
 
-function App() {
-  const [greeting, setGreeting] = useState('');
+// function App() {
+//   const [greeting, setGreeting] = useState('');
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    real_estate_app_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
+//   function handleSubmit(event) {
+//     event.preventDefault();
+//     const name = event.target.elements.name.value;
+//     real_estate_app_backend.greet(name).then((greeting) => {
+//       setGreeting(greeting);
+//     });
+//     return false;
+//   }
 
+//   return (
+//     <main>
+//       <img src="/logo2.svg" alt="DFINITY logo" />
+//       <br />
+//       <br />
+//       <form action="#" onSubmit={handleSubmit}>
+//         <label htmlFor="name">Enter your name: &nbsp;</label>
+//         <input id="name" alt="Name" type="text" />
+//         <button type="submit">Click Me!</button>
+//       </form>
+//       <section id="greeting">{greeting}</section>
+//     </main>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react'
+
+const App = () => {
   return (
-    <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
-    </main>
-  );
+    <div className='text-3xl'>App</div>
+  )
 }
 
-export default App;
+export default App
